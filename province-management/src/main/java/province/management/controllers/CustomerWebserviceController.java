4a8b7672d4a034f423680f7d88f2ba12f5faef32
Xuantho01 <hoxuantho95@gmail.com>
2020-05-12 17:33:12 +0700

    crud customer

--- province-management/src/main/java/province/management/controllers/CustomerWebserviceController.java
+++ province-management/src/main/java/province/management/controllers/CustomerWebserviceController.java
@@ -1,12 +1,11 @@
 package province.management.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import province.management.model.Customer;
 import province.management.service.CustomerService;
 import province.management.service.DuplicateEmailException;
@@ -19,22 +18,64 @@ public class CustomerWebserviceController {
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping("/API/customer")
-    public ResponseEntity<List<Customer>> listAllCustomer(){
+    @GetMapping("/api/customers")
+    public ResponseEntity<List<Customer>> listAllCustomer(UriComponentsBuilder ucBuilder){
         List<Customer> customers = customerService.findAll();
         if (customers.isEmpty()){
             return new ResponseEntity<List<Customer>>(HttpStatus.NO_CONTENT);
         }
 //        return res;
         ResponseEntity<List<Customer>> res = new ResponseEntity(customers, HttpStatus.OK);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/customers/{Id}").buildAndExpand(customers.get(1).getId()).toUri());
+
         return res;
     }
 
-    @PostMapping("/API/customer")
+    @PostMapping("/api/customers")
     public ResponseEntity<Void> showAllCustomer(@RequestBody Customer customer) throws DuplicateEmailException {
         customerService.save(customer);
         ResponseEntity<Void> res = new ResponseEntity(HttpStatus.OK);
         return res;
     }
+
+    @PutMapping("/api/customers/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable ("id") long id,@RequestBody Customer customer) throws Exception {
+        Customer currentCustomer = customerService.findById(id);
+
+        if (currentCustomer == null) {
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND); }
+
+        customer.setId(id);
+
+        customerService.save(customer);
+
+        return new ResponseEntity<Customer>(currentCustomer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/delete/customers/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable ("id") long id) throws Exception {
+
+        Customer currentCustomer = customerService.findById(id);
+
+        if (currentCustomer == null) {
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND); }
+
+        customerService.remove(id);
+
+        return new ResponseEntity<Customer>(currentCustomer, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/api/findCustomer/customers/{id}")
+    public ResponseEntity<Customer> listAllCustomers(@PathVariable("id") long id) throws Exception {
+        Customer customer = customerService.findById(id);
+        if (customer == null) {
+            System.out.println("Customer with id " + id + " not found");
+            return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+    }
+
 
 }
